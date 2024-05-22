73c3ae64421536d35bac9044af22888a9d558135
F4zination <minecraft.neot@gmail.com>
2024-05-22 22:37:57 +0200

    first stable http-request with response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// Create a default reedline object to handle user input
+
+use reqwest;
+use tokio;
+
+
+#[tokio::main]
+async fn main() {
+   
+   
+    let client = reqwest::Client::new();
+    let res = client.get("http://127.0.0.1:5000/blackboards")
+        .send().await
+        .unwrap();
+    println!("Status: {}", res.status());
+    println!("Body:\n{}", res.text().await.unwrap());
+
 }
