47b660f6f35dd69da754cddbe56ae762f1445845
F4zination <minecraft.neot@gmail.com>
2024-05-24 18:21:33 +0200

    Added the getBlackboards Method Committed for syncing with laptop

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 // Create a default reedline object to handle user input
 
 use reqwest;
+use std;
 use tokio;
-
 
 #[tokio::main]
 async fn main() {
-   
-   
+    let ip = "192.168.2.159";
+
+    get_blackboards(ip.to_string()).await;
+}
+
+async fn get_blackboards(ip: String) {
+    let route = std::format!("http://{}/blackboards", ip);
     let client = reqwest::Client::new();
-    let res = client.get("http://127.0.0.1:5000/blackboards")
-        .send().await
-        .unwrap();
+    let res = client.get(route).send().await.unwrap();
     println!("Status: {}", res.status());
     println!("Body:\n{}", res.text().await.unwrap());
-
 }
