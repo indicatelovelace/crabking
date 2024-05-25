c247b08950803d378a80fe039a7a568de19bfba3
F4zination <minecraft.neot@gmail.com>
2024-05-25 11:54:23 +0200

    Added some routes Committed for sync

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-// Create a default reedline object to handle user input
-
 use reqwest;
 use std;
 use tokio;
+use json;
 
 #[tokio::main]
 async fn main() {
-    let ip = "192.168.2.159";
+    let ip = "127.0.0.1:5000";
 
     get_blackboards(ip.to_string()).await;
 }
@@ -15,6 +14,30 @@ async fn get_blackboards(ip: String) {
     let route = std::format!("http://{}/blackboards", ip);
     let client = reqwest::Client::new();
     let res = client.get(route).send().await.unwrap();
+    println!("Status: {}", res.status());
+    println!("Body:\n{}", res.text().await.unwrap());
+}
+
+async fn del_blackboards(ip: String) {
+    let route = std::format!("http://{}/blackboards", ip);
+    let client = reqwest::Client::new();
+    let res = client.delete(route).send().await.unwrap();
+    println!("Status: {}", res.status());
+    println!("Body:\n{}", res.text().await.unwrap());
+}
+
+async fn get_blackboards_specific(ip: String, name: String) {
+    let route = std::format!("http://{}/blackboards/{}", ip, name);
+    let client = reqwest::Client::new();
+    let res = client.get(route).send().await.unwrap();
+    println!("Status: {}", res.status());
+    println!("Body:\n{}", res.text().await.unwrap());
+}
+
+async fn post_blackboards(ip: String, name: String, duration: i32) {
+    let route = std::format!("http://{}/blackboards", ip);
+    let client = reqwest::Client::new();
+    let res = client.post(route).json(&json!({"name": name, "duration": duration.to_string()})).send().await.unwrap();
     println!("Status: {}", res.status());
     println!("Body:\n{}", res.text().await.unwrap());
 }
